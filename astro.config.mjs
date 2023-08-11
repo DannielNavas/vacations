831123d9f94f6bafcad5f537e63336aeebf2ca6a
Danniel Navas <dannielnavas@gmail.com>
2023-08-10 21:11:05 -0500

    ✨ robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import tailwind from "@astrojs/tailwind";
-import vue from "@astrojs/vue";
 import { defineConfig } from 'astro/config';
-
+import vue from "@astrojs/vue";
 import sitemap from "@astrojs/sitemap";
+
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://vacations.danniel.dev/",
-  integrations: [tailwind(), vue(), sitemap(), robotsTxt()],
+  integrations: [tailwind(), vue(), sitemap(), robotsTxt()]
 });
